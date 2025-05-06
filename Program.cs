dd3bdec8b50b91dff4f3d5275ba4531bff269427
Liam Visser (1000134800) <VISSLC1@student.op.ac.nz>
2025-05-06 15:33:26 +1200

    Added spaces for everyone to type

--- Program.cs
+++ Program.cs
@@ -4,7 +4,71 @@
     {
         static void Main()
         {
+            //Liam types here
             Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Mohammad types here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Michael
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Jod types here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
