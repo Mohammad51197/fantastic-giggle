915d75669cfa313837b81862ba56def80d657fc6
Michael Lyon (1000134866) <LYONMA1@student.op.ac.nz>
2025-05-06 15:41:06 +1200

    Michael has added his part

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TeamCSFile
+﻿using System;
+
+namespace TeamCSFile
 {
     internal class Program
     {
@@ -38,18 +40,18 @@
 
 
             //Michael
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.WriteLine("Hello, my name is Michael and I'm working in a group project.");
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Look at me, I can count!");
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("I also can count down!");
+            Console.WriteLine("Goodbye.");
 
 
 
