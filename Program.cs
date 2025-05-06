2ca3fc9f7ace480fa3d810cf9e03107a638a349a
Akaljot Sodhi (1000129527) <SODHA2@student.op.ac.nz>
2025-05-06 15:43:16 +1200

    Something about myself

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TeamCSFile
+﻿using System;
+
+namespace TeamCSFile
 {
     internal class Program
     {
@@ -53,11 +55,21 @@
 
 
 
-            //Jod types here
+            //Jot types here
+            Console.WriteLine("Hello my name is Akaljot but you can call me jot.");
+            Console.WriteLine("I am from India from a state called Punjab and I came here as a international student to start my career in IT field");
+            Console.WriteLine("So my Idea about game project is to make a sci-fi game which will include ...............");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j <=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
 
 
-
-
+            Console.ReadLine();
 
 
 
